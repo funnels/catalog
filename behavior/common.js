1be5a91a06e627690409b080cd5266472f0f4fc6
sanvovanych <sanvovanych@gmail.com>
2017-03-29 03:11:24 +0300

    panel and popups

--- behavior/common.js
+++ behavior/common.js
@@ -28,6 +28,71 @@ jQuery(document).ready(function() {
 	});
 
 
+	// fix panel
+	function fixPanel(){
+		var $panel = $('.top-panel')
+			, negativeIndent = parseInt($('.box.box-subheader').css('margin-top'))
+
+			, panelTop = $('.site-header').outerHeight(true) 
+										+ $('.box.box-subheader').outerHeight(true)
+										- $panel.height()
+
+			, panelTopXs = $('.site-header').outerHeight(true) 
+										+ $('.box.box-subheader').outerHeight(true)
+										- negativeIndent
+
+			, flagToggle = true
+			;
+
+		if ($(window).scrollTop() >= panelTop) {
+			$panel.addClass('_show');
+			if (flagToggle) {
+				$('.toggle-absolute').fadeIn();
+				flagToggle = false;  
+			}
+		} else {
+			$panel.removeClass('_show');
+		}
+
+		flagToggle = true;
+
+		if ($(window).scrollTop() >= panelTopXs) {
+			$panel.addClass('_show-xs');
+			if (flagToggle) {
+				$('.toggle-absolute').fadeIn();
+				flagToggle = false;  
+			}
+		} else {
+			$panel.removeClass('_show-xs');
+		}
+	}
+
+	fixPanel();
+
+
+	// popup
+	$('.popup-link').magnificPopup({
+	  closeMarkup: '<div class="mfp-close">'+
+									'<svg class="icon-close"><use xlink:href="images/icons-vector.svg#close"></use></svg>'+
+		            '</div>',
+	  fixedContentPos: true,
+		preloader: false,
+		closeBtnInside: true,
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in'
+  });
+
+
+	// window scroll
+	$(window).scroll(function(){
+
+		// top panel
+		fixPanel();
+
+	});
+
+
 	// outside click
 	$('body').click(function(e){
 
